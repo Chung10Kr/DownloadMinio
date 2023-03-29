5a208f4a77fd9bc305b4ddb7fc50634842c8f169
Chung10Kr <30648191+Chung10Kr@users.noreply.github.com>
2023-03-29 23:35:00 +0900

    Update app.js
    Minio 관련 코드 외 제거

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 const Minio = require('minio')
-const readline = require("readline");
 
 class DownloadObj
 {
@@ -10,68 +9,22 @@ class DownloadObj
         this.accessKey = 'accessKey',
         this.secretKey = 'secretKey'
         this.bucketsNm = "Bucket Name";
-        this.path = null;
+        this.path = null; //폴더경로 , prefix
         this.minioClient = null;
         this.run();
     }
 
     async run()
     {
-        this.initInput();
-        await this.input();
         this.init();
         this.objDown();
-
-    }
-
-    async input()
-    {
-        let sInputed;
-        while(true){
-            sInputed = await this.readInput(`다운받을 경로를 입력해 주세요. ex) 폴더명/폴더명 \n`        );
-            if(sInputed){
-               break; 
-            };
-        }
-        this.path = sInputed;
-    }
-
-    initInput()
-    {
-        this.rl = readline.createInterface({
-            input: process.stdin,
-            output: process.stdout
-        });
-    }
-
-    async readInput(sMessage,bHide = false)
-    {
-        let self = this;
-        return (new Promise(function(resolve,reject) {
-            self.rl.question(sMessage,function(sInputed) {
-                resolve(sInputed);
-            });
-            self.rl._writeToOutput = function(stringToWrite) {
-                if (bHide) {
-                    self.rl.output.write("*");
-                }
-                else {
-                    self.rl.output.write(stringToWrite);
-                }
-            };
-        })).then(function(v){
-            self.rl._writeToOutput = function(stringToWrite) {
-                self.rl.output.write(stringToWrite);
-            };
-            return v;
-        });
     }
 
     init()
     {
         let self = this;
         this.minioClient = new Minio.Client({
-            endPoint: self.endPoint,
+            endPoint:self.endPoint,
             port: self.port,
             useSSL: false,
             accessKey: self.accessKey,
